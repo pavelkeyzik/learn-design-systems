9a351dc4596ad9356fa25e67efa25a681a06e05d
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-10-21 20:47:17 +0300

    Remove Icon wrappers

--- src/assets/icons/MailIcon.tsx
+++ src/assets/icons/MailIcon.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
 function MailIcon() {
   return (
-    <MailIconWrapper width="28" height="22" viewBox="0 0 28 22">
+    <svg width="28" height="22" viewBox="0 0 28 22">
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M0.666748 2.99999C0.666748 1.52724 1.86066 0.333328 3.33341 0.333328H24.6667C26.1395 0.333328 27.3334 1.52724 27.3334 2.99999V19C27.3334 20.4728 26.1395 21.6667 24.6667 21.6667H3.33342C1.86066 21.6667 0.666748 20.4728 0.666748 19V2.99999ZM24.6667 5.49133V19H3.33341V5.49134L14.0001 12.6024L24.6667 5.49133ZM23.5964 2.99999H4.4038L14.0001 9.39751L23.5964 2.99999Z"
         fill="currentColor"
       />
-    </MailIconWrapper>
+    </svg>
   );
 }
-
-const MailIconWrapper = styled.svg`
-  display: flex;
-  align-items: center;
-  vertical-align: middle;
-  width: 100%;
-  height: 100%;
-`;
 
 export { MailIcon };
