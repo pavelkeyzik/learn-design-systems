6d2f44c9b6225e8f0bec87e62a59b133377b84ed
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-10-22 23:29:27 +0300

    Move Typography stories to design tokens section

--- stories/Typography.stories.tsx
+++ stories/Typography.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { H1, H2, H3, H4, H5, P } from '../src/components/Typography';
 
 export default {
-  title: 'Components/Typography',
+  title: 'Design Tokens/Typography',
 };
 
 export function TypeScale() {
